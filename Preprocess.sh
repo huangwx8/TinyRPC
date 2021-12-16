192c1cde78f7923151a31eb4bc3f938867dca467
wexuanhuang <wexuanhuang@tencent.com>
2021-12-16 14:36:46 +0800

    Add a new shell script to view g++ macro expansion

---
+++ Preprocess.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+# Stop after the preprocessing stage; do not run the compiler proper.  The output is in the form of preprocessed source code, which is sent to the standard output.
+g++ -I ./include/ -E ./source/example/EchoClient.cc -o ./echoclient.i
